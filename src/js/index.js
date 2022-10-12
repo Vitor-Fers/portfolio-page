9e9e79a14455f1a666cc54eb71f91d2a9212dd3d
DevelVitor <vitorfers02@outlook.com>
2022-10-12 01:13:44 -0300

    Vinculação a api do github adicionada.

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+async function getApi(){
+    const apiResponse = await fetch(`https://api.github.com/users/DevelVitor`)
+    return await apiResponse.json()
+}
+
+async function perfil(){
+    let perfilName = document.getElementById("namePerfil")
+    let perfilImage = document.getElementById("perfil-img")
+    const api = await getApi()
+
+    perfilImage.src = api.avatar_url
+    perfilName.innerText = api.name
+    console.log(api.name)
+}
+perfil()
