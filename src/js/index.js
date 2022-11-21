1e2429cdaf73085a2c80bc54be4cfe7a4b27a59e
DevelVitor <vitorfers02@outlook.com>
2022-11-21 04:59:49 -0300

    Adicionado 'Carregando' enquanto aguarda entrada de dados da api.

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,49 @@
-async function getApi(){
+async function getApi() {
     const apiResponse = await fetch(`https://api.github.com/users/Vitor-Fers`)
     return await apiResponse.json()
 }
 
-
-async function getRepo(){
+async function getRepo() {
     const reposApi = await fetch(`https://api.github.com/users/offmak/repos`)
     return await reposApi.json()
 }
-    
 
-async function perfil(){
+async function perfil() {
     let perfilName = document.getElementById("namePerfil")
     let perfilImage = document.getElementById("perfil-img")
+
+    perfilName.innerText = 'Carregando...'
+
     const api = await getApi()
+    console.log(api)
 
-    perfilImage.src = api.avatar_url
-    perfilName.innerText = api.name
+    if (api) {
+        perfilImage.src = api.avatar_url
+        perfilName.innerText = api.name
+    }
 }
-perfil()
 
-async function projectsInfos(){
-    var projects = document.getElementById("projetos")
+async function projectsInfos() {
+    let projects = document.getElementById("projetos")
+
+    projects.innerHTML = 'Carregando...'
+
     const repos = await getRepo()
-    console.log(repos)
 
-    // let objetoRepos = Object.keys(repos).length
-    // console.log(objetoRepos)
+    if (repos) {
+        projects.innerHTML = null
+    }
 
-     repos.forEach((dado,index) => {
-        
+    repos.forEach((dado, index) => {
+
         let lang = (repos[index].language)
         let descript = (repos[index].description)
 
-        if(lang === null) lang = "linguagem Indisponível"
-        if(descript === null) descript = "Descrição Indisponível"
+        if (!lang) lang = "linguagem Indisponível"
+        if (!descript) descript = "Descrição Indisponível"
 
-        if(index < 4){
-    
+        if (index < 4) {
+
             projects.innerHTML += `<div class="cards_projects project">
             <span id="project" class="name_project">${repos[index].name}</span>
             <span class="public">${repos[index].visibility}</span>
@@ -45,8 +51,9 @@ async function projectsInfos(){
             <span id="lang" class="language">${lang}</span>
         </div>`
         }
-        }
+    }
     );
 }
 
+perfil()
 projectsInfos()
